b108d6af50e88290b58957b625503ff6aceb3128
Nathan <nytallards@gmail.com>
2025-11-20 16:57:51 +0500

    ok

--- core_support/models.py
+++ core_support/models.py
@@ -1,4 +1,3 @@
-import uuid
 from django.db import models
 
 
@@ -7,7 +6,6 @@ class Neighborhood(models.Model):
     Represents a neighborhood/mahalla in the system.
     """
     id = models.BigAutoField(primary_key=True)
-    neighborhood_uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     
     name_uz = models.CharField(max_length=128, db_index=True)  # Uzbek name
     name_ru = models.CharField(max_length=128, blank=True, null=True)  # Russian name
@@ -21,7 +19,6 @@ class Neighborhood(models.Model):
         verbose_name = "Neighborhood"
         verbose_name_plural = "Neighborhoods"
         indexes = [
-            models.Index(fields=["neighborhood_uuid"]),
             models.Index(fields=["name_uz"]),
         ]
         ordering = ['name_uz']
